78ac616a5be8005fc2a4d76e9325ca5b75cb5807
Federico Ulfo <federicoulfo@gmail.com>
2017-08-10 17:23:25 -0400

    Update Cassandra Persistent Volume

--- start-cassandra.sh
+++ start-cassandra.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+gcloud compute disks create cassandra-disk-0 --size=200GB
+gcloud compute disks create cassandra-disk-1 --size=200GB
+gcloud compute disks create cassandra-disk-2 --size=200GB
+
 kubectl create -f cassandra-storage-class.yml
 kubectl create -f cassandra-peer-service.yml
 kubectl create -f cassandra-service.yml
